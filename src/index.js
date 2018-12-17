ce1d3d8b271cac474258ebfa9495311237a79cc1
Chehlsee <chelseaciprich@gmail.com>
2018-12-16 22:26:38 -0500

    going to be up all night

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// this isn't connecting and I am not sure why not 
+import "./compontents/index.css";
+
+// to render a React element into a root DOM node, pass both to ReactDOM.render()
+ReactDOM.render(<App />, 
+  // when root is called everything inside it will be managed by React DOM
+  document.getElementById ("root"));
